b7104a04eeee7eb9ee2591ac41abcc1244887d36
LuisRicardoCardoso <143561575+LuisRicardoCardoso@users.noreply.github.com>
2023-10-16 19:34:42 -0300

    CALENDÁRIO 3° ATUALIZAÇÃO

---
+++ calendario/menu.h
@@ -0,0 +1,12 @@
+void menu()
+{
+    printf("\n======= Bem - vindo ao Calendário =======");
+    printf("\nSelecione alguma das opções do menu abaixo: ");
+    printf("\n\n[1]. Qual o dia da semana ?");
+    printf("\n[2]. Quantos dias para o fim do mês ?");
+    printf("\n[3]. Quantos dias para o Natal ?");
+    printf("\n[4]. Quantos dias para o Ano Novo ?");
+    printf("\n[5]. Qual ano é bissexto ?");
+    printf("\n[6]. Encerrar sistema\n");
+    printf("\nSua resposta: ");
+}
